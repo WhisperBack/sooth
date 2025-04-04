fb4526f789bd0728a4bcafc1dd9b0935254edd3b
WhisperBack <jdavis@thesoothapp.com>
2025-04-04 17:12:23 -0400

    Fix duplicate borderRadius on nav bar

--- components/ExperienceHeading.tsx
+++ components/ExperienceHeading.tsx
@@ -63,7 +63,7 @@ export default function ExperienceHeading() {
             width="100%"
             borderRadius="3xl"
             
-            border="1px solid rgba(255, 255, 255, 0.1)"
+            boxShadow="0 4px 30px rgba(0, 0, 0, 0.2)" border="1px solid rgba(255, 255, 255, 0.1)"
           >
             {[
               { href: '/about', label: 'About' },
