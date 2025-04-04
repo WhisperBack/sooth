8ef8f6024ef9d751b1ff4fce6fc5e7f8c4c60263
WhisperBack <jdavis@thesoothapp.com>
2025-04-04 17:18:54 -0400

    Remove shadow and preserve clean rounded nav bar

--- components/ExperienceHeading.tsx
+++ components/ExperienceHeading.tsx
@@ -63,7 +63,7 @@ export default function ExperienceHeading() {
             width="100%"
             borderRadius="3xl"
             
-            boxShadow="inset 0 0 0 1px rgba(255, 255, 255, 0.05)" border="1px solid rgba(255, 255, 255, 0.1)"
+             border="1px solid rgba(255, 255, 255, 0.1)"
           >
             {[
               { href: '/about', label: 'About' },
