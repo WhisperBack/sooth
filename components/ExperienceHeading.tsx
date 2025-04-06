b7b066342f81f1f98cab15a7f0a7b3426cc5ca6f
WhisperBack <jdavis@thesoothapp.com>
2025-04-06 15:51:36 -0400

    Add pulseGlow animation to Experience background for breathing effect

--- components/ExperienceHeading.tsx
+++ components/ExperienceHeading.tsx
@@ -137,15 +137,16 @@ export default function ExperienceHeading() {
   position="relative"
 >
   <MotionBox
-    position="absolute"
-    top="50%"
-    left="50%"
-    transform="translate(-50%, -50%)"
-    w="200%"
-    h="200%"
-    bgGradient="radial(at center, rgba(255,255,255,0.05), transparent)"
-    filter="blur(160px)"
-    zIndex={-1}
+  position="absolute"
+  top="50%"
+  left="50%"
+  transform="translate(-50%, -50%)"
+  w="200%"
+  h="200%"
+  bgGradient="radial(at center, rgba(255,255,255,0.05), transparent)"
+  filter="blur(160px)"
+  zIndex={-1}
+  animation="pulseGlow 12s ease-in-out infinite"
   />
   <Box
     as="span"
