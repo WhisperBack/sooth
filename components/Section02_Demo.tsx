4846c85cb3a032fbd2b609a942e6c812a2f7e01a
WhisperBack <jdavis@thesoothapp.com>
2025-04-07 19:27:42 -0400

    Remove unused transcript and response state to resolve ESLint warnings

--- components/Section02_Demo.tsx
+++
@@ -1,7 +0,0 @@
-// This file is no longer used but retained for future component reuse
-// All unused imports have been removed to prevent ESLint warnings
-
-export default function Section02_Demo() {
-    return null;
-  }
-  
