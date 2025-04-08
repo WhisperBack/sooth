55518f07fc8ca1a1ab3d02b416c41f038a159e16
WhisperBack <jdavis@thesoothapp.com>
2025-04-07 23:34:51 -0400

    Remove Section01_Hero to set Experience Sooth as true hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import Section01_Hero from '@/components/Section01_Hero';
-import Section02_Demo from '@/components/Section02_Demo';
 import Section03_Features from '@/components/Section03_Features';
+import Section02_Demo from '@/components/Section02_Demo';
 import Section04_Story from '@/components/Section04_Story';
 import Section05_Footer from '@/components/Section05_Footer';
 
 export default function Home() {
   return (
     <main>
-      <Section01_Hero />
-      <Section02_Demo />
       <Section03_Features />
+      <Section02_Demo />
       <Section04_Story />
       <Section05_Footer />
     </main>
