e17e9b70f5a9dad4e664af78feb89b0d73c1e0cd
WhisperBack <jdavis@thesoothapp.com>
2025-04-09 19:58:36 -0400

    Fix global CSS import location for Vercel build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
-import '../styles/globals.css';
+
 
 export default function Home() {
   const [showSubline, setShowSubline] = useState(false);
